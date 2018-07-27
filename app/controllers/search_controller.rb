afa87e2b23e5202928e0021c74d507ee5b4ca1f7
Adib <adibalamir@hotmail.com>
2018-07-27 18:21:51 +0000

    controller part of search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,16 @@
 class SearchController < ApplicationController
+  def index
+    @pets = Pet.where(nil)
+    filtering_params(params).each do |key, value|
+      @pets = @pets.public_send(key, value) if value.present?
+    end
+  end
+
   def show
+  end
+
+  private
+  def filtering_params(params)
+    params.slice(:city, :animal_type, :breed, :gender, :size, :age, :status)
   end
 end
