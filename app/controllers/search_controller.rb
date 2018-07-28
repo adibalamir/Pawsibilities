846237859515ea1d3217f31124b18048ad003ed6
Theresa Campbell <t.campbell@sasktel.net>
2018-07-28 17:27:22 +0000

    If form value is an empty string, it is not included in the query. Selective querying fixed!

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,20 +1,30 @@
 class SearchController < ApplicationController
   def index
-    @pets = Pet.where(
+    query = {
       city: params[:city],
       animal_type: params[:animal_type],
-      breed: params[:breed],
-      gender: params[:gender],
-      age: params[:age],
-      size: params[:size])
+    }
+
+
+    if (!params[:size].nil? && !params[:size].empty?)
+      query[:size] = params[:size]
+    end
+
+    if (!params[:breed].nil? && !params[:breed].empty?)
+      query[:breed] = params[:breed]
+    end
+
+    if (!params[:gender].nil? && !params[:gender].empty?)
+      query[:gender] = params[:gender]
+    end
+
+    if (!params[:age].nil? && !params[:age].empty?)
+      query[:age] = params[:age]
+    end
+
+    @pets = Pet.where(query)
   end
 
   def show
   end
-
- private
-  def search_params
-    params.permit(:city, :animal_type, :breed, :gender, :size, :age)
-  end
 end
-
