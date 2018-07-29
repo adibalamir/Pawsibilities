41f0669ac6a5d69e615f28e2c857ffc3d037edde
Theresa Campbell <t.campbell@sasktel.net>
2018-07-29 21:49:13 +0000

    Trying to access shelter's information to render it on the pet profile page. Work in progress.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get "/" => "home#index"
 
   resources :pets, only: [:show]
-    # resource :dog_highlight, only: [:show]
 
   resources :match, only: [:index, :create]
 
