af4fbf9308d4413cf4886f1e653aa3f6b2dad4a3
Theresa Campbell <t.campbell@sasktel.net>
2018-07-29 19:10:09 +0000

    Generated model for shelters and job for downloading them.

---
+++ app/jobs/download_shelters_job.rb
@@ -0,0 +1,7 @@
+class DownloadSheltersJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_28_221238) do
+ActiveRecord::Schema.define(version: 2018_07_29_190111) do
 
   create_table "dog_breed_traits", force: :cascade do |t|
     t.string "breed"
@@ -74,6 +74,19 @@ ActiveRecord::Schema.define(version: 2018_07_28_221238) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["petfinder_id"], name: "index_pets_on_petfinder_id"
+  end
+
+  create_table "shelters", force: :cascade do |t|
+    t.string "name"
+    t.string "shelter_id"
+    t.string "email"
+    t.string "phone"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shelter_id"], name: "index_shelters_on_shelter_id"
   end
 
 end
