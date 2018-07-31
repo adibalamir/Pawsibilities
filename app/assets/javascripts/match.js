193ea4cb5fe9785b2a539b31f991d922b3cd3efa
thoumc <thoumc@hotmail.com>
2018-07-31 20:20:57 +0000

    form added

---
+++ app/assets/javascripts/match.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+
+
+  $("article:nth-child(1n+2)").hide();
+
+
+  $("article").first().addClass("visible").css("display", "block");
+
+
+  //every time the next button is clicked, remove the current panels class of visible and apply it to the next and fade it in.
+  $("button.next").on("click", function(e){
+    e.preventDefault();
+    $(this).closest("article").removeClass("visible").hide().next().addClass("visible").fadeIn();
+
+  });
+  //every time the prev button is clicked, remove the current panels class of visible and apply it to the prev and fade it in.
+    $("button.prev").on("click", function(e){
+    e.preventDefault();
+    $(this).closest("article").removeClass("visible").hide().prev().addClass("visible").fadeIn();
+
+  });
+
+})
+
