d138c9f790f96de091daa24438f28cbe5d7fec9e
Theresa Campbell <t.campbell@sasktel.net>
2018-08-01 14:45:46 +0000

    Working on getting checkboxes and their labels inline with each other.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,19 +1,7 @@
 $(document).ready(function() {
 
   $(".dropdown-toggle").click(function() {
-     $(this).siblings(".dropdown-menu").slideToggle();
+     $(this).siblings(".dropdown-menu").slideToggle("fast");
   });
 
 });
-
-
- // <div class="search-parameter">
- //      <button type=button class="reveal-checkbox-container">Gender</button>
- //      <div class="checkbox-container">
- //        <% genders = ["M", "F"] %>
- //        <% genders.each do |gender| %>
- //          <%= check_box_tag("gender[]", value = gender, @selected_genders.include?(gender)) %>
- //          <%= label_tag(gender) %> <br/>
- //        <% end %>
- //      </div>
- //    </div>
