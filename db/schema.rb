63a01a442cdec3461207a63f6b702b605e2156b3
thoumc <thoumc@hotmail.com>
2018-08-01 16:29:43 +0000

    user model updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_184741) do
+ActiveRecord::Schema.define(version: 2018_08_01_160114) do
 
   create_table "cat_highlights", force: :cascade do |t|
     t.string "breed"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 2018_07_30_184741) do
     t.boolean "found", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
