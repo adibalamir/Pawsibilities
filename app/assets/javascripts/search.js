e5ef2bc0b3e44903031d3418c88a6363f1f3a2ee
Theresa Campbell <t.campbell@sasktel.net>
2018-08-04 20:41:36 +0000

    Fixed bug where checkboxes weren't clickable during filtering.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -6,7 +6,7 @@ $(document).on('turbolinks:load', function() {
      });
   });
 
-  $(".dropdown-item").click(function() {
+  $("body").on('click', ".dropdown-item", function() {
     if ($(this).children("input").is(":checked")) {
       $(this).children("input").prop('checked', false)
     } else {
@@ -14,7 +14,7 @@ $(document).on('turbolinks:load', function() {
     }
   });
 
-  $("input").click(function() {
+  $("body").on('click', "input", function() {
     if ($(this).is(":checked")) {
       $(this).prop('checked', false)
     } else {
