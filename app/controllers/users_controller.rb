aa45b11dde830316ade738a9bbd538f5ca536202
Theresa Campbell <t.campbell@sasktel.net>
2018-08-04 16:44:47 +0000

    After login or registration, user is redirected to the adoption page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to '/search'
     else
       flash[:error] = "An error occured!"
 
