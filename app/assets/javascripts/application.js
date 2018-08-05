90dfc5382018a2f031d8b9db17a6bee0534c0ca9
Theresa Campbell <t.campbell@sasktel.net>
2018-08-05 21:59:38 +0000

    Working on making nav bar responsive.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,5 +18,20 @@
 //= require_tree .
 //= require match
 //= require search
+//= require cable
+//= require shelters
 
 
+//Making nav bar responsive - work in progress!
+$(document).on('turbolinks:load', function() {
+
+  $('nav ul li > a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+
+});
