76608160ba3c334a0791193ee9a69d4edaa11b62
Adib <adibalamir@hotmail.com>
2018-08-07 21:53:56 +0000

    merging

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,13 +2,13 @@ class Shelter < ApplicationRecord
   :authenticate_with_credentials
   has_secure_password
 
-  validates_uniqueness_of :shelter_id
-  validates_presence_of :name, :email, :address, :phone
+  # validates_uniqueness_of :shelter_id
+  # validates_presence_of :name, :email, :address, :phone
 
-  def self.authenticate_with_credentials(email, password)
-    email.downcase!
-    user = Shelter.find_by_email(email.strip)
-    return user && user.authenticate(password)
-  end
+  # def self.authenticate_with_credentials(email, password)
+  #   email.downcase!
+  #   user = Shelter.find_by_email(email.strip)
+  #   return user && user.authenticate(password)
+  # end
 
 end
